7839e3b28244d97f2c7b5c50d11d69d333fd50b3
Soraiya11-7 <soraiyaparvin@gmail.com>
2025-01-16 02:24:59 +0600

    Implement participant registration with duplicate entry validation and update the participant count for the associated camp

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,11 +32,45 @@ async function run() {
     //all collections............................
     const userCollection = client.db("medicalCampDB").collection("users");
     const campCollection = client.db("medicalCampDB").collection("camps");
+    const participantCollection = client.db("medicalCampDB").collection("participants");
+
+
+    
+    //register-participant.............
+
+    app.post('/register-participant', async (req, res) => {
+      const item = req.body;
+
+      // Check if the participant has already joined the specific camp.........
+      const query = { participantEmail: item.participantEmail, campId: item.campId };
+      const existingParticipant = await participantCollection.findOne(query);
+
+      if (existingParticipant) {
+        return res.send({ message: 'Participant already joined this camp', insertedId: null });
+      }
+      const result = await participantCollection.insertOne(item);
+
+
+       // Update the participant count in the camp document.........
+      const camp = await campCollection.findOne({ _id: new ObjectId(item.campId) });
+      // if (!camp) {
+      //   return res.status(404).send({ message: "Camp not found" });
+      // }
+
+      const filter = { _id: new ObjectId(item.campId) };
+      const updateDoc = {
+        $inc: { participants: 1 },
+      };
+      const updateResult = await campCollection.updateOne(filter, updateDoc);
+      // if (updateResult.modifiedCount === 0) {
+      //   return res.status(500).send({ message: "Failed to update participant count" });
+      // }
+      res.send(result);
+    });
 
 
 
     //camp  related api....................................................
-
 
     app.get('/camps', async (req, res) => {
       const result = await campCollection.find().toArray();
@@ -71,7 +105,7 @@ async function run() {
     app.get('/users/admin/:email', async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
-      
+
       const user = await userCollection.findOne(query);
       let admin = false;
       if (user) {
@@ -95,9 +129,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send("server is running...");
-  })
-  
-  app.listen(port, () => {
-    console.log(`server is running on port : ${port}`);
-  })
+  res.send("server is running...");
+})
+
+app.listen(port, () => {
+  console.log(`server is running on port : ${port}`);
+})
